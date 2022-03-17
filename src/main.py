3791978a84620286caaebea7edf711968120e212
Onyx39 <valentin.richard39@sfr.fr>
2022-03-17 18:31:36 +0100

    improve main

--- src/main.py
+++ src/main.py
@@ -7,14 +7,19 @@ import create_Huffman_tree as ht
 alphabet_file_path = 'C:/Users/val_p/Desktop/PROJ631/1_Python_Decompression_Huffman/exemple_freq.txt'
 compressed_file_path = 'C:/Users/val_p/Desktop/PROJ631/1_Python_Decompression_Huffman/exemple_comp.bin'
 
-alphabet_file = open(alphabet_file_path, 'r')
-lines_of_file = alphabet_file.readlines()
+def create_huffman_tree (alphabet_file_path, compressed_file_path) :
+    alphabet_file = open(alphabet_file_path, 'r')
+    lines_of_file = alphabet_file.readlines()
 
-compressed_file = open(compressed_file_path, 'r')
+    compressed_file = open(compressed_file_path, 'r')
 
-alphabet_dictionnary = gc.create_alphabet_dictionary(lines_of_file)
-huffman_tree = ht.create_Huffman_tree(ht.create_all_trees(alphabet_dictionnary))
+    alphabet_dictionnary = gc.create_alphabet_dictionary(lines_of_file)
+    huffman_tree = ht.create_Huffman_tree(ht.create_all_trees(alphabet_dictionnary))
 
-print(huffman_tree)
+    return(huffman_tree)
+
+print(create_huffman_tree(alphabet_file_path, compressed_file_path))
+
+
 
 
