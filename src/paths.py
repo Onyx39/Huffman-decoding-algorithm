522208ca6d23114ad2ed868479db36e0d90bc305
Valentin RICHARD <90321867+Onyx39@users.noreply.github.com>
2022-04-25 17:13:29 +0200

    Update paths.py

--- src/paths.py
+++ src/paths.py
@@ -3,10 +3,10 @@
 #This file is imported in the main file.
 
 
-#Enter the path to get the file that contains the alphabet of the text.
+#Enter the path to the file that contains the alphabet of the text.
 alphabet_file_path = 'C:/Users/val_p/Desktop/PROJ631/1_Python_Decompression_Huffman/exemple_freq.txt'
 
-#Enter the path to get the file that contains the compressed text.
+#Enter the path to the file that contains the compressed text.
 compressed_file_path = 'C:/Users/val_p/Desktop/PROJ631/1_Python_Decompression_Huffman/exemple_comp.bin'
 
 #Enter the path to the place you want to put the decoded message.
